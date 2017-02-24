5e2d5ce26bd8c9ac549e595b9825bfdeb8cc0ec3
nekithockey75 <nikita01051997@gmail.com>
2017-02-24 20:43:48 +0500

    window works

--- Algorithms/GUIGeoLab.java
+++ Algorithms/GUIGeoLab.java
@@ -20,13 +20,20 @@ import javax.swing.JFrame;
 	public void paint(Graphics g) {
 		Graphics2D gr2d = (Graphics2D) g;
 
+		int height = 0, width = 0;
+
 		gr2d.setPaint(Color.MAGENTA);
 		for (Figure figure : f) {
 			Polygon j = new Polygon();
-			for (Vertex v : figure.vert_array)
+			for (Vertex v : figure.vert_array) {
+				width = (v.x > width ? v.x : width);
+				height = (v.y > height ? v.y : height);
 				j.addPoint(100 * v.y, 100 * v.x);
+			}
 			g.drawPolygon(j);
 		}
+
+		setSize(100 * height, 100 * width + 100);
 
 		//gr2d.clearRect(50, 40, 200, 200);
 	}
